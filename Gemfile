9b542d18727af9caf499879c8558abc25dcb21e4
Alex <alex@alex-pc.(none)>
2012-01-13 17:59:43 +0200

    init commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+
+gem 'mysql2'
+gem 'json'
+gem 'haml'
+gem 'mongrel'
+gem 'rake', '0.8.7'
+gem 'lazybox'
+gem 'facebook_oauth'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem "rspec-rails"
+  gem 'factory_girl'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetable::Application.config.secret_token = 'b46ef3a074c988c7cedf3c3849c4cc3706d9f6a9c4d17fd4b65c42531d2f8b7237bb7a44ead23bbe414f5a7d81770a4cb7f9a0789d86c6acd1ad228e6aa41526'
