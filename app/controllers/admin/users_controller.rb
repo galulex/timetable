237b50e06cb1a8292d213792184a29bf84f3c85b
Alexey <alexey@alexey-pc.(none)>
2012-01-14 17:23:07 +0200

    Facebook

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,7 +9,11 @@ class Admin::UsersController < ApplicationController
   end
 
   def create
-
+    client = FacebookOAuth::Client.new(
+      :application_id => '273919792672642',
+      :application_secret => '73278582e486a6c2358b451128f2b385',
+      :callback => 'http://example.com/facebook/callback'
+    )
   end
 
 end
